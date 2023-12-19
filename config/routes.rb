ad783ac92b9c7f94adb15e58189800abe8957cff
Paul Gamarra <paulgamarra.dev@gmail.com>
2023-12-19 20:48:35 -0300

    completado el controller users métodos teachers(index) student(index) user(show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/teachers'
-  get 'users/students'
+  root to: "pages#home"
 
   devise_for :users
 
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'profesores', to: "users#teachers", as: :teachers
+  get 'comunidad', to: "users#students", as: :students
+  get 'perfil/:id', to: "users#show", as: :user
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
