c3286343e07d859a9b8416ffb376750ebb8da9f3
Paul Gamarra <paulgamarra.dev@gmail.com>
2024-01-06 15:27:33 -0300

    pgsearch gem implemented for User Model, added search field (form_with) at students.html.erb, and modified User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def teachers
     @teachers = User.where(role: "teacher")
+    if params[:query].present?
+      @teachers = @teachers.search_by_name_location_and_languages(params[:query])
+    end
   end
 
   def students
     @students = User.all
+    if params[:query].present?
+      @students = @students.search_by_name_location_and_languages(params[:query])
+    end
   end
 
   def show
