fac62b9dacffa1f0d3200f05bb78eebcdbed7ccb
wind peña <nirvanavid@gmail.com>
2024-01-17 22:37:30 -0300

    correcion def show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  
   def teachers
     @teachers = User.where(role: "teacher")
     if params[:query].present?
@@ -18,4 +18,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
 end
