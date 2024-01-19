4d05dffb3c1c2b32cde8ba0d6256b2720f2301e7
Paul Gamarra <paulgamarra.dev@gmail.com>
2024-01-18 23:23:57 -0300

    corregido bug con profe Segundo, el problema estaba en el channel que volvimos a generar

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -10,8 +10,8 @@ export default class extends Controller {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       // { received: data => console.log(data) },
-      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data) },
-      // { received: data => this.#insertMessageAndScrollDown(data) }
+      // { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data) },
+      { received: data => this.#insertMessageAndScrollDown(data) }
     )
 
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js", preload: true
 # Añadido a través de terminal usando instrucción: importmap pin "@rails/actioncable" --from jsdelivr
 pin "@rails/actioncable", to: "https://cdn.jsdelivr.net/npm/@rails/actioncable@7.1.2/app/assets/javascripts/actioncable.esm.js"
+#pin "@rails/actioncable", to: "actioncable.esm.js"
